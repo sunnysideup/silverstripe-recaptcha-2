2c85988a82632da3080b0bf2b3a30e1cc18733af
Sebastian Kalicki <s.kalicki@whow.net>
2019-05-09 17:13:22 +0200

    Added ReCaptcha Protector class for SpamProtector.

---
+++ src/ReCaptchaProtector.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kmedia\ReCaptcha;
+
+use SilverStripe\SpamProtection\SpamProtector;
+
+class ReCaptchaProtector implements SpamProtector
+{
+    /**
+     * Return the Field that we will use in this protector
+     * @param string $name
+     * @param string $title
+     * @param null $value
+     * @return ReCaptchaField
+     */
+    public function getFormField($name = 'ReCaptchaField', $title = 'ReCaptcha', $value = null)
+    {
+        return ReCaptchaField::create($name, $title);
+    }
+
+    /**
+     * Not used by ReCaptcha
+     */
+    public function setFieldMapping($fieldMapping)
+    {
+    }
+}
