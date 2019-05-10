a870ea5cb7381afc958758c87d2d13b319c5bca8
Sebastian Kalicki <sebastian@kalicki.email>
2019-05-10 21:14:17 +0200

    Added onload callback and on submit function.

--- javascript/ReCaptchaField.js
+++ javascript/ReCaptchaField.js
@@ -1,1 +1,22 @@
-console.log('Hello World :)');
+'use strict';
+
+function reCaptchaOnloadCallback() {
+    var reCaptcha = document.querySelector('.g-recaptcha');
+    var form = reCaptcha.closest('form');
+
+    if (reCaptcha.dataset.size === 'invisible') {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            grecaptcha.execute();
+        });
+    }
+
+    grecaptcha.render(reCaptcha, reCaptcha.dataset);
+}
+
+function reCaptchaOnSubmit(token) {
+    var tokenField = document.querySelector('.g-recaptcha-response');
+    var form = tokenField.closest('form');
+    tokenField.value = token;
+    form.submit();
+}
