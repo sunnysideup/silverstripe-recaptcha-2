802a3ba948983bcb543dc5c3f496030a723b55e0
Sebastian Kalicki <sebastian@kalicki.email>
2019-05-13 20:10:26 +0200

    refactoring

--- javascript/domReady.js
+++ javascript/domReady.js
@@ -1,35 +1,39 @@
-'use strict';
+(function (window, document) {
+    'use strict';
 
-function domReady(fn) {
-    var fns = [],
-        loaded = domLoaded();
+    function domReady(fn) {
+        var fns = [],
+            loaded = domLoaded();
 
-    domAddEventListener(fns, loaded);
-    domHandleCallback(loaded, fns, fn);
-}
-
-function domLoaded() {
-    return (document.documentElement.doScroll ? /^loaded|^c/ : /^loaded|^i|^c/).test(document.readyState);
-}
-
-function domAddEventListener(fns, loaded) {
-    var listener;
-
-    if (!loaded) {
-        document.addEventListener('DOMContentLoaded', listener = function () {
-            document.removeEventListener('DOMContentLoaded', listener);
-            loaded = true;
-            while (listener = fns.shift()) {
-                listener();
-            }
-        })
+        domAddEventListener(fns, loaded);
+        domHandleCallback(loaded, fns, fn);
     }
-}
 
-function domHandleCallback(loaded, fns, fn) {
-    if (loaded) {
-        setTimeout(fn, 0);
-    } else {
-        fns.push(fn);
+    function domLoaded() {
+        return (document.documentElement.doScroll ? /^loaded|^c/ : /^loaded|^i|^c/).test(document.readyState);
     }
-}
+
+    function domAddEventListener(fns, loaded) {
+        var listener;
+
+        if (!loaded) {
+            document.addEventListener('DOMContentLoaded', listener = function () {
+                document.removeEventListener('DOMContentLoaded', listener);
+                loaded = true;
+                while (listener = fns.shift()) {
+                    listener();
+                }
+            })
+        }
+    }
+
+    function domHandleCallback(loaded, fns, fn) {
+        if (loaded) {
+            setTimeout(fn, 0);
+        } else {
+            fns.push(fn);
+        }
+    }
+
+    window.domReady = domReady;
+})(window, document);
