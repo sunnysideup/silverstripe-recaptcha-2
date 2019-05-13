83f84336694549f51ab7688bfff2cea8bd3953bf
Sebastian Kalicki <sebastian@kalicki.email>
2019-05-13 20:18:11 +0200

    refactoring

--- javascript/ReCaptchaField.js
+++ javascript/ReCaptchaField.js
@@ -1,34 +1,44 @@
 /** global: grecaptcha */
 
-'use strict';
+(function (window, document) {
+    'use strict';
 
-function reCaptchaInit() {
-    var element = document.createElement('script'),
-        target = document.querySelectorAll('script')[0],
-        protocol = 'https:' == document.location.protocol ? 'https' : 'http';
-    element.type = 'text/javascript';
-    element.src = protocol + '://www.google.com/recaptcha/api.js?onload=reCaptchaOnloadCallback&render=explicit&hl='.concat(window.SS_LOCALE);
-    target.parentNode.insertBefore(element, target);
-}
-
-function reCaptchaOnloadCallback() {
-    var reCaptcha = document.querySelector('.g-recaptcha');
-
-    if (reCaptcha.dataset.size === 'invisible') {
-        document.querySelector('#'.concat(window.ReCaptchaFormId)).addEventListener('submit', reCaptchaFormOnSubmit);
+    function reCaptchaInit() {
+        var element = document.createElement('script'),
+            target = document.querySelectorAll('script')[0],
+            protocol = 'https:' == document.location.protocol ? 'https' : 'http';
+        element.type = 'text/javascript';
+        element.src = protocol + '://www.google.com/recaptcha/api.js?onload=reCaptchaOnloadCallback&render=explicit&hl='.concat(window.SS_LOCALE);
+        target.parentNode.insertBefore(element, target);
     }
 
-    grecaptcha.render(reCaptcha, reCaptcha.dataset);
-}
+    function reCaptchaOnloadCallback() {
+        var reCaptcha = document.querySelector('.g-recaptcha');
 
-function reCaptchaOnSubmit(token) {
-    document.querySelector('#'.concat(window.ReCaptchaFormId, ' .g-recaptcha-response')).value = token;
-    document.querySelector('#'.concat(window.ReCaptchaFormId)).submit();
-}
+        if (reCaptcha.dataset.size === 'invisible') {
+            reCaptchaForm().addEventListener('submit', reCaptchaFormOnSubmit);
+        }
 
-function reCaptchaFormOnSubmit(event) {
-    event.preventDefault();
-    grecaptcha.execute();
-}
+        grecaptcha.render(reCaptcha, reCaptcha.dataset);
+    }
 
-domReady(reCaptchaInit);
+    function reCaptchaOnSubmit(token) {
+        document.querySelector('#'.concat(window.ReCaptchaFormId, ' .g-recaptcha-response')).value = token;
+        reCaptchaForm().submit();
+    }
+
+    function reCaptchaForm() {
+        return document.querySelector('#'.concat(window.ReCaptchaFormId));
+    }
+
+    function reCaptchaFormOnSubmit(event) {
+        event.preventDefault();
+        grecaptcha.execute();
+    }
+
+    domReady(reCaptchaInit);
+
+    window.reCaptchaOnloadCallback = reCaptchaOnloadCallback;
+    window.reCaptchaOnSubmit = reCaptchaOnSubmit;
+})(window, document);
+
